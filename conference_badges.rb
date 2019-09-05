92e252cde371c8650c57a50bcb9c730792a76ede
christian williams <christian.williamsj@gmail.com>
2019-09-05 18:14:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,8 +10,8 @@ end
 
 def assign_rooms(attendees)
   room=0 
-  attendees.collect do |name|
-  room+=1 
-  puts "Hello, #{name}! You'll be assigned to room #{room}!"
+    attendees.collect do |name|
+   room+=1 
+  "Hello, #{name}! You'll be assigned to room #{room}!"
  end 
 end  
