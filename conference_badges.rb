0dc9926a5dd74c067e5c88f6619ab3a092323376
christian williams <christian.williamsj@gmail.com>
2019-09-05 17:59:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(speakers)
-  speakers.do collect |name|
+def batch_badge_creator(attendees)
+  attendees.do collect |name|
   badge_maker(name)
 end 
 
